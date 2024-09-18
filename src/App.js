762e1fe48f01f40475b503858229cde6fab775f5
adamking314 <king.adam314@gmail.com>
2024-09-17 20:52:28 -0700

    home page edits

--- src/App.js
+++ src/App.js
@@ -18,24 +18,60 @@ const App = () => {
   return (
     <Router>
       <div className="container">
-        <h2>React Rotuer Demo</h2>
+        <h1>Currency converter </h1>
         <nav>
           <ul>
             <li>
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/about/">About</Link>
+              <Link to="/currency-converter/">Currency Converter</Link>
             </li>
             <li>
-              <Link to="/contact/">Contact</Link>
+              <Link to="/exchange-rate/">Exchange Rate</Link>
             </li>
           </ul>
         </nav>
+
         <Route path="/" exact component={Home} />
         <Route path="/about/" component={About} />
         <Route path="/contact/" component={Contact} />
+
+
+          <div className="amount">
+           <label> Amount: <input type="number"> </input> </label> 
+          </div>
+          <div className="from-to">
+            <div className="from">
+            <label> From: </label>
+                    <div class="dropdown">
+                       <button class="dropbtn">Dropdown</button>
+                        <div class="dropdown-content">
+                           <a href="#">USD</a>
+                           <a href="#">Link 2</a>
+                            <a href="#">Link 3</a>
+                       </div>
+                  </div>
+              </div>
+            <div className="to">
+            <label> To: </label>
+                      <div class="dropdown">
+                        <button class="dropbtn">Dropdown</button>
+                        <div class="dropdown-content">
+                            <a href="#">USD</a>
+                            <a href="#">Link 2</a>
+                            <a href="#">Link 3</a>
+                        </div>
+                      </div>
+                </div>
+          </div>
+          <div>
+            <p> Amount in currency</p>
+          </div>
       </div>
+      <footer>
+
+      </footer>
     </Router>
   );
 }
